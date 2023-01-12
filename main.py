f3637f1527d9d90a7ae79715d5e21cfa36b5e1cf
sudharaD <sudharad7@gmail.com>
2023-01-12 10:03:02 +0530

    TODOs added

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+# TODO: 1. Check the user’s input to decide what to do next.
 
+# TODO: 1.1. When the user enters “report” to the prompt, a report should be generated
+# TODO: Resource checker function
+
+# TODO: 1.2. When the user chooses a drink, the program should check if there are enough resources to make that drink.
+# TODO: If resources not enough - It should not continue to mak ethe drink but print: “Sorry there is not enough water.”
+
+# TODO: 1.3. If there are sufficient resources to make the drink
+# TODO: program should prompt the user to insert coins
+# TODO: Process coins function - (Create dictionary for hold the coin values)
+
+# TODO: 2. Check transaction successful?
+# TODO: Check that the user has inserted enough money to purchase the drink they selected
+# TODO: If it is not enough refund the money
+# TODO: If it enough -  ingredients to make the drink should be deducted. Give item to customer
+
+# TODO: 3. Profit and Balance Handle
+# TODO: Cost of the drink gets added to the machine as the profit
+# TODO: If the user has inserted too much money, the machine should offer change.
+
+# TODO: 4. Repeat this process
+
+# TODO: The prompt should show again to serve the next customer
+
+# TODO: Turn off the Coffee Machine by entering “off”
